935bea3817a2764f3f470395c1ff9c5d2c11dd52
daleyzou <daley.zou@qq.com>
2018-07-23 22:21:47 +0800

    LeetCode2

--- src/com/daleyzou/leetcode/LeetCode2.java
+++ src/com/daleyzou/leetcode/LeetCode2.java
@@ -1,14 +1,77 @@
 package com.daleyzou.leetcode;
 
-/**
- * @Author: DaleyZou
- * @Description:
- * @Date: Created in 15:33 2018-7-20
- * @Modified By:
- */
 public class LeetCode2 {
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+    }
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1.val == 0){
+            return l2;
+        }
+        if (l2.val == 0){
+            return l1;
+        }
+        ListNode listNode1 = new ListNode(0);
+        ListNode listNode_ = new ListNode(2);
+        listNode_ = listNode1;
+        while (l1 != null && l2 != null) {
+            ListNode temp = new ListNode(l1.val + l2.val);
+            listNode1.next = temp;
+
+            listNode1 = listNode1.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+//        if (l1.next == null && l2.next == null){
+//            l1.val += l2.val;
+//            listNode1.next = l1;
+//        }
+        while (l1 != null){
+            listNode1.next = l1;
+            l1 = l1.next;
+            listNode1 = listNode1.next;
+
+        }
+        while (l2 != null){
+            listNode1.next = l2;
+            l2 = l2.next;
+            listNode1 = listNode1.next;
+
+        }
+        if (listNode_.next != null) {
+            listNode_ = listNode_.next;
+        }
+        listNode1 = listNode_;
+        while (listNode1.next != null){
+            if (listNode1.val >= 10){
+                listNode1.next.val += 1;
+                listNode1.val -= 10;
+            }
+            listNode1 = listNode1.next;
+        }
+        if (listNode1.val >= 10){
+            listNode1.next = new ListNode(1);
+            listNode1.val -= 10;
+        }
+        return listNode_;
+    }
 
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode listNode = addTwoNumbers(l1, l2);
+
+
+
+        System.out.println("结束");
+        System.out.println(listNode);
     }
 }
--- src/com/daleyzou/leetcode/Leetcode1.java
+++ src/com/daleyzou/leetcode/Leetcode1.java
@@ -1,11 +1,11 @@
 package com.daleyzou.leetcode;
 
 
-public class Leetcode1 {
+public class LeetCode1 {
 
     /**
      * @Description: 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
-                        你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+    你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
      * @auther: DaleyZou
      * @date: 8:06 2018/7/20 
      * @param: nums
@@ -26,12 +26,12 @@ public class Leetcode1 {
         return null;
     }
 
-   public static void main(String[] args){
+    public static void main(String[] args){
         int[] nums = {2,4,6,8,1};
         int target = 9;
-       int[] ints = twoSum(nums, target);
-       for (int i = 0; i < ints.length; i++){
-           System.out.println(ints[i]);
-       }
-   }
+        int[] ints = twoSum(nums, target);
+        for (int i = 0; i < ints.length; i++){
+            System.out.println(ints[i]);
+        }
+    }
 }
