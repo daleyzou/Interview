33b13b39fb7813270bba76c4e26d0d94a342567c
daleyzou <daleyzou@163.com>
2018-07-25 18:37:41 +0800

    company

--- src/com/daleyzou/leetcode/LeetCode5.java
+++ src/com/daleyzou/leetcode/LeetCode5.java
@@ -16,16 +16,45 @@ public class LeetCode5 {
      * @return: java.lang.String
      */
     public String longestPalindrome(String s) {
-        String[] str = new String[1000];
-        char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length; i++){
-            
+        if (s.length() < 2){
+            return s;
+        }
+        String result = s.substring(0,1);
+        String temp = null;
+        for (int i = 0; i < s.length(); i++){
+            temp = null;
+            for (int j = s.length() -1; j > i; j--){
+                temp = s.substring(i, j);
+                if (isPalindrome(temp)){
+                    if (temp.length() > result.length()){
+                        result = temp;
+                    }
+                }
+            }
         }
 
-        return null;
+        return result;
+    }
+
+    public static boolean isPalindrome(String x) {
+        char[] chars = x.toCharArray();
+        int i = 0;
+        int j = chars.length - 1;
+        while (i != j && i < chars.length / 2){
+            if (chars[i] == chars[j]){
+                i++;
+                j--;
+            }else {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args){
-
+        LeetCode5 leetCode5 = new LeetCode5();
+        String s = "bb";
+        //String t = s.substring(0, 1);
+        System.out.println(leetCode5.longestPalindrome(s));
     }
 }
