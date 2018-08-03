3bd2868d19b052b7cd99ac02af625867cb0b7d21
daleyzou <daleyzou@163.com>
2018-08-03 18:09:20 +0800

    home

--- src/com/daleyzou/leetcode/LeetCode14.java
+++ src/com/daleyzou/leetcode/LeetCode14.java
@@ -8,9 +8,41 @@ package com.daleyzou.leetcode;
  */
 public class LeetCode14 {
 
+    /**
+     * @Description: 编写一个函数来查找字符串数组中的最长公共前缀。
+     *
+     * 如果不存在公共前缀，返回空字符串 ""。
+     * @auther: DaleyZou
+     * @date: 10:10 2018-8-3
+     * @param: strs
+     * @return: java.lang.String
+     */
     public String longestCommonPrefix(String[] strs) {
-
-        return null;
+        if (strs.length == 1) return strs[0];
+        if (strs.length >= 2) {
+            int i = 0;
+            String result = countCommonPrefix(strs[0], strs[1]);
+            if (result.equals("")) return result;
+            if (strs.length >=3) {
+                for (int j = 2; j < strs.length; j++) {
+                    result = countCommonPrefix(result, strs[j]);
+                    if (result.equals("")) break;
+                }
+            }
+            return result;
+        }
+        return "";
+    }
+    public String countCommonPrefix(String str1, String str2){
+        String result = "";
+        for (int i = 0; i < Math.min(str1.length(), str2.length()); i++){
+            if (str1.charAt(i) == str2.charAt(i)){
+                result += str1.charAt(i);
+            }else {
+                break;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args){
---
+++ src/com/daleyzou/leetcode/LeetCode16.java
@@ -0,0 +1,16 @@
+package com.daleyzou.leetcode;
+
+/**
+ * @Author: DaleyZou
+ * @Description:
+ * @Date: Created in 16:20 2018-8-3
+ * @Modified By:
+ */
+public class LeetCode16 {
+
+    public static void main(String[] args){
+        LeetCode16 leetCode16 = new LeetCode16();
+
+        System.out.println();
+    }
+}
