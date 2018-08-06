60829c51381bd8581ab03c2ec88ce12155dc97a6
daleyzou <daleyzou@163.com>
2018-08-06 18:35:18 +0800

    dd

--- src/com/daleyzou/leetcode/LeetCode16.java
+++ src/com/daleyzou/leetcode/LeetCode16.java
@@ -3,12 +3,21 @@ package com.daleyzou.leetcode;
 /**
  * @Author: DaleyZou
  * @Description:
+ *
+ * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ *
+ * 例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+ *
+ * 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
  * @Date: Created in 16:20 2018-8-3
  * @Modified By:
  */
 public class LeetCode16 {
 
+    public int threeSumClosest(int[] nums, int target) {
 
+        return 0;
+    }
 
     public static void main(String[] args){
         LeetCode16 leetCode16 = new LeetCode16();
