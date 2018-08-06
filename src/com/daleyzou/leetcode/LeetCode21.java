45cb53c711c93e82b29227fbccd38f5d64e18d03
daleyzou <daley.zou@qq.com>
2018-08-07 07:38:02 +0800

    home

--- src/com/daleyzou/leetcode/LeetCode21.java
+++ src/com/daleyzou/leetcode/LeetCode21.java
@@ -15,8 +15,30 @@ public class LeetCode21 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
+        ListNode result = new ListNode(0);
+        ListNode head = result;
+        while (l1 != null && l2 != null){
+            if (l1.val < l2.val){
+                result.next = l1;
+                l1 = l1.next;
+                result = result.next;
+            }else {
+                result.next = l2;
+                l2 = l2.next;
+                result = result.next;
+            }
+        }
+        while (l1 != null){
+            result.next = l1;
+            l1 = l1.next;
+            result = result.next;
+        }
+        while (l2 != null){
+            result.next = l2;
+            l2 = l2.next;
+            result = result.next;
+        }
 
-
-        return null;
+        return head.next;
     }
 }
