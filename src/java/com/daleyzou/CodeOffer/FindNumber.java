ae9e36bb353ac5709885fc9c9cb8ba2c8d4e0c5c
daleyzou <daleyzou@163.com>
2019-01-28 23:05:24 +0800

    开始剑指offer

---
+++ src/java/com/daleyzou/CodeOffer/FindNumber.java
@@ -0,0 +1,30 @@
+package com.daleyzou.CodeOffer;
+
+/**
+ * @Author: DaleyZou
+ * @Description: 二维数组中的查找
+ * 在一个二维数组中（每个一维数组的长度相同），
+ * 每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * @Date: Created in 23:38 2019/1/17
+ * @Modified By:
+ */
+public class FindNumber {
+
+    /**
+     * @Description: 二维数组中的查找
+     * @date: 23:46 2019/1/17
+     * @param: target
+     * @param: array
+     * @return: boolean
+     */
+    public boolean Find(int target, int [][] array) {
+
+        return true;
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("hello ");
+    }
+}
---
+++ src/java/com/daleyzou/CodeOffer/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @Author: DaleyZou
+ * @Description: 这个包下所写的题都是剑指off题目
+ * @Date: Created in 23:39 2019/1/17
+ * @Modified By:
+ */
+package com.daleyzou.CodeOffer;
