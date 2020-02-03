fc678f039d7c66787c3b69af2aa973554c1fa6c1
daleyzou <daleyzou@163.com>
2020-02-03 23:47:40 +0800

    27

---
+++ src/java/com/daleyzou/CodeOffer/d27_Permutation.java
@@ -0,0 +1,25 @@
+package com.daleyzou.CodeOffer;
+
+import java.util.ArrayList;
+
+/**
+ * d27_Permutation
+ *
+ 题目描述
+ 输入一个字符串,按字典序打印出该字符串中字符的所有排列。
+ 例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+
+
+ 输入描述:
+ 输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。
+ * @author zoudaifa
+ * @date 2020年02月03日 23:46
+ * @version 3.2.0
+ */
+public class d27_Permutation {
+
+    public ArrayList<String> Permutation(String str) {
+
+        return null;
+    }
+}
