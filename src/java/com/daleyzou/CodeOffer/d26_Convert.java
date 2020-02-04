edce8989bc68f716fd34806f1fb1deac4c0c8b09
daleyzou <daleyzou@163.com>
2020-02-04 18:59:18 +0800

    27

--- src/java/com/daleyzou/CodeOffer/d26_Convert.java
+++ src/java/com/daleyzou/CodeOffer/d26_Convert.java
@@ -11,34 +11,36 @@ package com.daleyzou.CodeOffer;
 public class d26_Convert {
     public class TreeNode {
         int val = 0;
+
         TreeNode left = null;
+
         TreeNode right = null;
 
         public TreeNode(int val) {
             this.val = val;
-
         }
 
     }
+
     public TreeNode Convert(TreeNode root) {
-        if (root == null){
+        if (root == null) {
             return null;
         }
-        if (root.left == null && root.right == null){
+        if (root.left == null && root.right == null) {
             return root;
         }
         TreeNode left = Convert(root.left);
         TreeNode p = left;
-        while (p!=null && p.right!=null){
+        while (p != null && p.right != null) {
             p = p.right;
         }
-        if (left != null){
+        if (left != null) {
             p.right = root;
             root.left = p;
         }
 
         TreeNode right = Convert(root.right);
-        if (right!=null){
+        if (right != null) {
             right.left = root;
             root.right = right;
         }
--- src/java/com/daleyzou/CodeOffer/d27_Permutation.java
+++ src/java/com/daleyzou/CodeOffer/d27_Permutation.java
@@ -1,6 +1,11 @@
 package com.daleyzou.CodeOffer;
 
+import com.sun.org.apache.bcel.internal.generic.SWAP;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * d27_Permutation
@@ -12,14 +17,59 @@ import java.util.ArrayList;
 
  输入描述:
  输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。
- * @author zoudaifa
+ * @author daleyzou
  * @date 2020年02月03日 23:46
  * @version 3.2.0
  */
 public class d27_Permutation {
 
-    public ArrayList<String> Permutation(String str) {
+    ArrayList<String> resultList = new ArrayList<>();
 
-        return null;
+    public ArrayList<String> Permutation(String str) {
+        char[] chars = str.toCharArray();
+        getAllArrangement(chars, 0);
+        Collections.sort(resultList);
+        return resultList;
+    }
+
+    private void getAllArrangement(char[] chars, int begin) {
+        if (chars == null || chars.length <= 0) {
+            return;
+        }
+        if (begin == chars.length - 1) {
+            resultList.add(String.valueOf(chars));
+            return;
+        }
+        // 第一个一次和后面的进行交换
+        for (int j = begin; j < chars.length; j++) {
+            if (isSwap(chars, begin, j)) {
+                swap(chars, begin, j);
+                getAllArrangement(chars, begin + 1);
+                swap(chars,begin, j);
+            }
+        }
+    }
+
+    private boolean isSwap(char[] chars, int i, int length) {
+        for (int j = i; j < length; j++) {
+            if (chars[i] == chars[length]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void swap(char[] chars, int i, int j) {
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        d27_Permutation object = new d27_Permutation();
+        ArrayList<String> a = object.Permutation("abc");
+        a.stream().forEach(System.out::println);
+
     }
 }
