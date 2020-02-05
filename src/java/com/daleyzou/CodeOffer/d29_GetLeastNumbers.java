61d3373cfd6f7a6cc63e429c19b1cd9f18ca0f7e
daleyzou <daleyzou@163.com>
2020-02-05 09:10:52 +0800

    GetLeastNumbers_Solution

---
+++ src/java/com/daleyzou/CodeOffer/d29_GetLeastNumbers.java
@@ -0,0 +1,25 @@
+package com.daleyzou.CodeOffer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * d29_GetLeastNumbers
+ * @description 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+ * @author zoudaifa
+ * @date 2020年02月04日 20:24
+ * @version 3.2.0
+ */
+public class d29_GetLeastNumbers {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList<Integer> resultList = new ArrayList<>();
+        if (input== null || input.length <= 0 || k > input.length){
+            return resultList;
+        }
+        Arrays.sort(input);
+        resultList = new ArrayList<>(Arrays.stream(input).boxed().collect(Collectors.toList()).subList(0, k));
+        return resultList;
+    }
+}
