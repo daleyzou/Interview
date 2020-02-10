acfd3f41bf82036ea073329513a582acee708943
daleyzou <daleyzou@163.com>
2020-02-10 23:01:26 +0800

    NumberOf1Between1AndN_Solution

--- src/java/com/daleyzou/CodeOffer/d29_GetLeastNumbers.java
+++ src/java/com/daleyzou/CodeOffer/d29_GetLeastNumbers.java
@@ -8,6 +8,11 @@ import java.util.stream.Collectors;
 /**
  * d29_GetLeastNumbers
  * @description 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+ *
+ * https://github.com/apachecn/awesome-algorithm/tree/master/docs/%E5%89%91%E6%8C%87offer/Java/40_KLeastNumbers
+ *
+ * https://blog.csdn.net/shakespeare001/article/details/51280814
+ *
  * @author daleyzou
  * @date 2020年02月04日 20:24
  * @version 3.2.0
--- src/java/com/daleyzou/CodeOffer/d31_NumberOf1Between1AndN.java
+++ src/java/com/daleyzou/CodeOffer/d31_NumberOf1Between1AndN.java
@@ -6,13 +6,39 @@ package com.daleyzou.CodeOffer;
  * 求出1~13的整数中1出现的次数,并算出100~1300的整数中1出现的次数？
  * 为此他特别数了一下1~13中包含1的数字有1、10、11、12、13因此共出现6次,但是对于后面问题他就没辙了。
  * ACMer希望你们帮帮他,并把问题更加普遍化,可以很快的求出任意非负整数区间中1出现的次数（从1 到 n 中1出现的次数）
+ *
+ * https://blog.csdn.net/yi_Afly/article/details/52012593
+ *
+ *round  weight   former  base
+ * 5     3        4
+ * 如果 weight = 0，count=round*base
+ * 如果 weight=1,count=round*base+1+former
+ * 如果weight>1,count=round*base+base
+ *
+ *
  * @author daleyzou
  * @date 2020年02月06日 20:56
  * @version 3.2.0
  */
 public class d31_NumberOf1Between1AndN {
     public int NumberOf1Between1AndN_Solution(int n) {
+        int count = 0;
+        int base = 1;
+        int round = n;
+        while (round > 0) {
+            int weight = round % 10;
+            round = round / 10;
+            count += round * base;
+            if (weight > 0) {
+                if (weight == 1) {
+                    count += n % base + 1;
+                } else {
+                    count += base;
+                }
+            }
+            base *= 10;
+        }
 
-        return 0;
+        return count;
     }
 }
