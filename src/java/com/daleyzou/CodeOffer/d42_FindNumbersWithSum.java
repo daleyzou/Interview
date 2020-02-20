42062f52d3bd3d11b0327d96165f3bcfec5b43a8
DaleyZou's World <daleyzou@163.com>
2020-02-20 09:06:44 +0800

    FindNumbersWithSum

---
+++ src/java/com/daleyzou/CodeOffer/d42_FindNumbersWithSum.java
@@ -0,0 +1,19 @@
+package com.daleyzou.CodeOffer;
+
+import java.util.ArrayList;
+
+/**
+ * d42_FindNumbersWithSum
+ * @description 输入一个递增排序的数组和一个数字S，在数组中查找两个数，使得他们的和正好是S，
+ * 如果有多对数字的和等于S，输出两个数的乘积最小的
+ * @author daleyzou
+ * @date 2020年02月20日 9:05
+ * @version 1.1.11
+ */
+public class d42_FindNumbersWithSum {
+    public ArrayList<Integer> FindNumbersWithSum(int [] array,int sum) {
+        ArrayList<Integer> resultList = new ArrayList<>();
+
+        return resultList;
+    }
+}
