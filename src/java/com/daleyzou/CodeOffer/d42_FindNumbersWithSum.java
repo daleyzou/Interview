7177a7a97ff672ccc2e5450441080f69c5ea6e83
DaleyZou's World <daleyzou@163.com>
2020-02-21 00:19:33 +0800

    FindNumbersWithSum

--- src/java/com/daleyzou/CodeOffer/d42_FindNumbersWithSum.java
+++ src/java/com/daleyzou/CodeOffer/d42_FindNumbersWithSum.java
@@ -13,7 +13,52 @@ import java.util.ArrayList;
 public class d42_FindNumbersWithSum {
     public ArrayList<Integer> FindNumbersWithSum(int [] array,int sum) {
         ArrayList<Integer> resultList = new ArrayList<>();
-
+        for (int i = 0; i <array.length; i++){
+            int temp = sum - array[i];
+            if (temp <= array[i] || temp > array[array.length - 1]){
+                continue;
+            }
+            Integer index = findNum(temp, array, i, array.length - 1);
+            if (index > i){
+                if (resultList.size() == 2){
+                    int num1 = resultList.get(0);
+                    int num2 = resultList.get(1);
+                    if (array[i] * array[index] < num1 * num2){
+                        resultList.clear();
+                        resultList.add(array[i]);
+                        resultList.add(array[index]);
+                    }
+                }else {
+                    resultList.add(array[i]);
+                    resultList.add(array[index]);
+                }
+            }
+        }
         return resultList;
+    }
+
+    private Integer findNum(int num,int [] array, int begin, int end) {
+        if (begin>end){
+            return -1;
+        }
+        if (num < array[begin] || num > array[end]){
+            return -1;
+        }
+        int middle = (begin + end) / 2;
+        if (num == array[middle]){
+            return middle;
+        }
+        if (num>array[middle]){
+            return findNum(num, array, middle + 1, end);
+        }else {
+            return findNum(num, array, begin, middle - 1);
+        }
+    }
+
+    public static void main(String[] args){
+        d42_FindNumbersWithSum object = new d42_FindNumbersWithSum();
+        int[] array = new int[]{1,2,4,7,11,15};
+        ArrayList<Integer> integers = object.FindNumbersWithSum(array, 15);
+        System.out.println(integers);
     }
 }
