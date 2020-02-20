84564bb0809a84d5a5d01517e87d6a71eaef59e4
DaleyZou's World <daleyzou@163.com>
2020-02-21 00:21:15 +0800

    LeftRotateString

---
+++ src/java/com/daleyzou/CodeOffer/d43_LeftRotateString.java
@@ -0,0 +1,19 @@
+package com.daleyzou.CodeOffer;
+
+/**
+ * d43_LeftRotateString
+ * @description 汇编语言中有一种移位指令叫做循环左移（ROL），现在有个简单的任务，
+ * 就是用字符串模拟这个指令的运算结果。对于一个给定的字符序列S，
+ * 请你把其循环左移K位后的序列输出。
+ * 例如，字符序列S=”abcXYZdef”,要求输出循环左移3位后的结果，即“XYZdefabc”。是不是很简单
+ * @author daleyzou
+ * @date 2020年02月21日 0:19
+ * @version 1.1.11
+ */
+public class d43_LeftRotateString {
+    public String LeftRotateString(String str,int n) {
+        String result = "";
+
+        return result;
+    }
+}
