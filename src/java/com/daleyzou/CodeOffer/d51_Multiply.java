88f6a83241c15ae11523da8841651aec53ab7b9c
zoudaifa <daleyzou@gmail.com>
2020-05-07 09:15:08 +0800

    the second methon of multiply

--- src/java/com/daleyzou/CodeOffer/d51_Multiply.java
+++ src/java/com/daleyzou/CodeOffer/d51_Multiply.java
@@ -4,7 +4,7 @@ package com.daleyzou.CodeOffer;
  * d51_Multiply
  * @description
  *
- * 给定一个数组A[0,1,...,n-1],
+ * 给定一个数组A[0, 1, ..., n-1],
  * 请构建一个数组B[0,1,...,n-1],其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。
  * 不能使用除法。（注意：规定B[0] = A[1] * A[2] * ... * A[n-1]，B[n-1] = A[0] * A[1] * ... * A[n-2];）
  *
@@ -22,10 +22,10 @@ public class d51_Multiply {
      */
     public int[] multiply(int[] A) {
         int[] result = new int[A.length];
-        for (int i = 0; i < A.length; i++){
+        for (int i = 0; i < A.length; i++) {
             int count = 1;
-            for (int j = 0; j < A.length; j++){
-                if (i == j){
+            for (int j = 0; j < A.length; j++) {
+                if (i == j) {
                     continue;
                 }
                 count = count * A[j];
@@ -34,5 +34,30 @@ public class d51_Multiply {
         }
 
         return result;
+    }
+
+    /**
+     *
+     * B[i]的意义是A数组不包括i位置的所有乘积，分为 i左边的元素乘积和 i右边的所有元素乘积。
+     * 第一个for计算i左边的乘积，第二个for计算右边的。初始化B[0]=1，是因为0左边没有元素，所以乘积为1。
+     *
+     */
+    public int[] multiply_2(int[] A) {
+        int length = A.length;
+        int[] B = new int[length];
+        if (length != 0) {
+            // 计算下三角连乘
+            B[0] = 1;
+            for (int i = 1; i < length; i++) {
+                B[i] = B[i - 1] * A[i-1];
+            }
+            int temp = 1;
+            // 计算上三角连乘
+            for (int j = length - 2; j >= 0; j--) {
+                temp *= A[j + 1];
+                B[j] *= temp;
+            }
+        }
+        return B;
     }
 }
