95b89b6941172d0913e4e6d5411a741fba56251d
zoudaifa <daleyzou@gmail.com>
2020-11-17 21:55:04 +0800

    add match

--- src/java/com/daleyzou/CodeOffer/d52_Match.java
+++ src/java/com/daleyzou/CodeOffer/d52_Match.java
@@ -1,7 +1,5 @@
 package com.daleyzou.CodeOffer;
 
-import java.util.Arrays;
-
 /**
  * d52_Math
  * @description
@@ -22,7 +20,7 @@ public class d52_Match {
 
     public boolean match_1(char[] str, char[] pattern) {
         boolean result = false;
-        if (str.length == 0 && pattern.length == 0){
+        if (str.length == 0 && pattern.length == 0) {
             return true;
         }
         int strLength = str.length;
@@ -33,38 +31,37 @@ public class d52_Match {
 
     private boolean matchMore(char[] str, int strIndex, char[] pattern, int patternIndex) {
         // 都到了末尾
-        if (strIndex == str.length && pattern.length == patternIndex){
+        if (strIndex == str.length && pattern.length == patternIndex) {
             return true;
         }
         // 字符串没有到末尾， 模式已经到了末尾
-        if (pattern.length == patternIndex && strIndex != str.length){
+        if (pattern.length == patternIndex && strIndex != str.length) {
             return false;
         }
 
         // 第二个字符是 *
-        if (patternIndex + 1 < pattern.length && pattern[patternIndex + 1] == '*' ){
+        if (patternIndex + 1 < pattern.length && pattern[patternIndex + 1] == '*') {
             // 当前是相等 或者 .   开始考虑三种情况
-            if (strIndex != str.length && pattern[patternIndex] == '.' || strIndex != str.length && pattern[patternIndex] == str[strIndex]){
-                return matchMore(str, strIndex, pattern, patternIndex+2) ||
-                        matchMore(str, strIndex + 1, pattern, patternIndex + 2) ||
-                        matchMore(str, strIndex + 1, pattern, patternIndex);
-            }else{
+            if (strIndex != str.length && pattern[patternIndex] == '.'
+                    || strIndex != str.length && pattern[patternIndex] == str[strIndex]) {
+                return matchMore(str, strIndex, pattern, patternIndex + 2) || matchMore(str, strIndex + 1, pattern, patternIndex + 2)
+                        || matchMore(str, strIndex + 1, pattern, patternIndex);
+            } else {
                 return matchMore(str, strIndex, pattern, patternIndex + 2);
             }
         }
 
         //  判断当前匹配是 .或者相等
-        if (str.length != strIndex && (pattern[patternIndex] == str[strIndex] || pattern[patternIndex] == '.')){
+        if (str.length != strIndex && (pattern[patternIndex] == str[strIndex] || pattern[patternIndex] == '.')) {
             return matchMore(str, strIndex + 1, pattern, patternIndex + 1);
-        }else {
+        } else {
             return false;
         }
 
     }
 
-    public boolean match_2(char[] str, char[] pattern)
-    {
-        if (pattern.length <= 0){
+    public boolean match_2(char[] str, char[] pattern) {
+        if (pattern.length <= 0) {
             return str.length <= 0;
         }
         String s = str.length == 0 ? "" : String.valueOf(str);
@@ -73,36 +70,74 @@ public class d52_Match {
     }
 
     private boolean checkIsMatch(String s, String p) {
-        if (p.length() <= 0){
+        if (p.length() <= 0) {
             return s.length() <= 0;
         }
-        boolean isMatch = s.length() >0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
-        if (p.length() > 1 && p.charAt(1) == '*'){
-            return checkIsMatch(s, p.substring(2)) || (isMatch && s.length() >0 && checkIsMatch(s.substring(1), p.substring(2)))
-                    || (isMatch && s.length() > 0 && checkIsMatch(s.substring(1), p));
-        }else {
+        boolean isMatch = s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+        if (p.length() > 1 && p.charAt(1) == '*') {
+            return checkIsMatch(s, p.substring(2)) || (isMatch && s.length() > 0 && checkIsMatch(s.substring(1), p.substring(2))) || (
+                    isMatch && s.length() > 0 && checkIsMatch(s.substring(1), p));
+        } else {
             return isMatch && checkIsMatch(s.substring(1), p.substring(1));
         }
     }
 
-    public boolean match(char[] str, char[] pattern)
-    {
-        if (pattern.length <= 0){
+    /**
+     *  https://www.cnblogs.com/mfrank/p/10472663.html
+     *
+     * @param str
+     * @param pattern
+     * @return
+     * @author daleyzou
+     */
+    public boolean match_3(char[] str, char[] pattern) {
+        if (pattern.length <= 0) {
             return str.length <= 0;
         }
+        if (str.length == 0) {
+            return pattern.length == 2 && pattern[0] == '.' && pattern[1] == '*';
+        }
+        Boolean[][] result = new Boolean[str.length + 1][pattern.length + 1];
+        String s = String.valueOf(str);
+        String p = String.valueOf(pattern);
+        return dynamicMatch(0, 0, s, p, result);
+    }
 
+    private boolean dynamicMatch(int sBegin, int pBegin, String s, String p, Boolean[][] result) {
+        if (result[sBegin][pBegin] != null) {
+            return result[sBegin][pBegin].equals(Boolean.TRUE);
+        }
+        boolean ans = false;
+        if (p.length() == pBegin) {
+            ans = s.length() == sBegin;
+        }
+        if (pBegin < p.length()) {
+            boolean currentMatch = sBegin != s.length() && (s.charAt(sBegin) == p.charAt(pBegin) || p.charAt(pBegin) == '.');
+            if (pBegin + 1 < p.length() && p.charAt(pBegin + 1) == '*') {
+                ans = dynamicMatch(sBegin, pBegin + 2, s, p, result) || (currentMatch && dynamicMatch(sBegin + 1, pBegin, s, p, result));
+            } else {
+                ans = currentMatch && dynamicMatch(sBegin + 1, pBegin + 1, s, p, result);
+            }
+        }
+        result[sBegin][pBegin] = ans;
+        return ans;
+    }
+
+    public boolean match(char[] str, char[] pattern)
+    {
 
     }
 
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         d52_Match match = new d52_Match();
 
-        System.out.println("true " + match.match("aaa".toCharArray(),"a*a".toCharArray()));
+        System.out.println("true " + match.match("aaa".toCharArray(), "a*a".toCharArray()));
 
-        System.out.println("true " + match.match("".toCharArray(),".*".toCharArray()));
+        System.out.println("true " + match.match("".toCharArray(), ".*".toCharArray()));
         // "a","ab*a"
-        System.out.println("false " + match.match("a".toCharArray(),"ab*a".toCharArray()));
+        System.out.println("false " + match.match("a".toCharArray(), "ab*a".toCharArray()));
 
-        System.out.println("false " + match.match("aa".toCharArray(),"b*a".toCharArray()));
+        System.out.println("false " + match.match("aa".toCharArray(), "b*a".toCharArray()));
     }
 }
