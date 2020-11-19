8a5d38e0735f8c72e68824d882d107888ad535d2
zoudaifa <daleyzou@gmail.com>
2020-11-19 21:36:58 +0800

    add isNumeric

---
+++ src/java/com/daleyzou/CodeOffer/d54_FirstApearingOnce.java
@@ -0,0 +1,44 @@
+package com.daleyzou.CodeOffer;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * d54_FirstApearingOnce
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年11月19日 20:59
+ * @version 1.1.1
+ */
+public class d54_FirstApearingOnce {
+//    Map<Character, Integer> tempMap = new LinkedHashMap<>();
+//    public void Insert(char ch)
+//    {
+//        if (tempMap.containsKey(ch)){
+//            tempMap.remove(ch);
+//        }else {
+//            tempMap.put(ch, 1);
+//        }
+//    }
+//    //return the first appearence once char in current stringstream
+//    public char FirstAppearingOnce()
+//    {
+//        if (tempMap.size() == 0){
+//            return '#';
+//        }else {
+//           return tempMap.entrySet().iterator().next().getKey();
+//        }
+//    }
+//Insert one char from stringstream
+public void Insert(char ch)
+{
+
+}
+    //return the first appearence once char in current stringstream
+    public char FirstAppearingOnce()
+    {
+
+    }
+}
