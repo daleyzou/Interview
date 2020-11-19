24d493eaf37d3698b766c3a290ddb6016a06453b
zoudaifa <daleyzou@gmail.com>
2020-11-19 20:58:53 +0800

    add isNumeric

--- src/java/com/daleyzou/CodeOffer/d53_IsNumeric.java
+++ src/java/com/daleyzou/CodeOffer/d53_IsNumeric.java
@@ -36,15 +36,72 @@ public class d53_IsNumeric {
         if (str.length == 0){
             return false;
         }
+        boolean isPoint = false;
+        int countSymble = 0;
+        int countE = 0;
+        for (int i = 0; i < str.length; i++){
+            char currentChar = str[i];
+            if (currentChar == '+' || currentChar == '-'){
+                countSymble++;
+                if (i >= str.length - 1){
+                    return false;
+                }
+                if (!isCurrentNumeric(str[ i + 1])){
+                    if (i == 0 && str[i+1]=='.'){
+                        continue;
+                    }
+                    return false;
+                }
+                if (i > 0){
+                    boolean b = str[i - 1] == 'e' || str[i - 1] == 'E';
+                    if (!b){
+                        return false;
+                    }
+                }
+            }else if (currentChar == '.'){
+                if (isPoint){
+                    return false;
+                }
+                if (i != str.length - 1 &&countE > 0){
+                    return false;
+                }
+                if (i != 0 &&i!=str.length - 1 && !(isCurrentNumeric(str[i - 1]) && isCurrentNumeric(str[i+1]))){
+                    if (i == 1 && isCurrentNumeric(str[i+1])){
+                        continue;
+                    }
+                    return false;
+                }
+                isPoint = true;
+            }else if (currentChar == 'E' || currentChar == 'e'){
+                countE++;
+                if (i == str.length - 1 || i == 0){
+                    return false;
+                }
+                if (!isCurrentNumeric(str[i - 1])){
+                    return false;
+                }
+            }else if (isCurrentNumeric(currentChar)){
+                continue;
+            }else {
+                return false;
+            }
+        }
+        if (countSymble > 2 || countE > 1){
+            return false;
+        }
 
-
-
-        return false;
+        return true;
     }
 
+    private boolean isCurrentNumeric(char currentChar) {
+        return currentChar >= '0' && currentChar <= '9';
+    }
 
     public static void main(String[] args){
         d53_IsNumeric test = new d53_IsNumeric();
-        System.out.println("-1E-16 : " + test.isNumeric("-1E-16".toCharArray()));;
+        System.out.println("-1E-16 : " + test.isNumeric("-1E-16".toCharArray()));
+        System.out.println("-.123 : " + test.isNumeric("-.123".toCharArray()));
+        System.out.println("1+23 : " + test.isNumeric("1+23".toCharArray()));
+        System.out.println("12e+5.4 : " + test.isNumeric("12e+5.4".toCharArray()));
     }
 }
