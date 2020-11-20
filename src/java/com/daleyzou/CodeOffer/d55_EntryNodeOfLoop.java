2100e8cc1928de80286f2987b10cd7c281c0b02f
daleyzou <daleyzou@163.com>
2020-11-20 13:40:28 +0800

    add EntryNodeOfLoop

---
+++ src/java/com/daleyzou/CodeOffer/d55_EntryNodeOfLoop.java
@@ -0,0 +1,25 @@
+package com.daleyzou.CodeOffer;
+
+/**
+ * d55_EntryNodeOfLoop
+ * @description 给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
+ * @author daleyzou
+ * @date 2020年11月20日 13:38
+ * @version 1.1.1
+ */
+public class d55_EntryNodeOfLoop {
+    public class ListNode {
+        int val;
+
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode EntryNodeOfLoop(ListNode pHead) {
+
+        return null;
+    }
+}
