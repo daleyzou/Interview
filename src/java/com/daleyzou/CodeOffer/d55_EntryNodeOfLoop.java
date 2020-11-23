8884c64051399f171ba84076c1fd269f2dc7c429
daleyzou <daleyzou@163.com>
2020-11-23 23:38:00 +0800

    add EntryNodeOfLoop

--- src/java/com/daleyzou/CodeOffer/d55_EntryNodeOfLoop.java
+++ src/java/com/daleyzou/CodeOffer/d55_EntryNodeOfLoop.java
@@ -1,5 +1,8 @@
 package com.daleyzou.CodeOffer;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * d55_EntryNodeOfLoop
  * @description 给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
@@ -18,8 +21,31 @@ public class d55_EntryNodeOfLoop {
         }
     }
 
-    public ListNode EntryNodeOfLoop(ListNode pHead) {
-
+    /**
+     *  使用一个 set 来做去重判断
+     *
+     * @param pHead
+     * @return
+     * @author daleyzou
+     */
+    public ListNode EntryNodeOfLoop_1(ListNode pHead) {
+        if (pHead == null || pHead.next == null){
+            return null;
+        }
+        Set<ListNode> sets = new HashSet<>();
+        sets.add(pHead);
+        ListNode tempNode;
+        while ((pHead = pHead.next) != null){
+            if (sets.contains(pHead)){
+                return pHead;
+            }
+            sets.add(pHead);
+        }
         return null;
+    }
+
+    public ListNode EntryNodeOfLoop(ListNode pHead)
+    {
+
     }
 }
