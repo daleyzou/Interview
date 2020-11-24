55ac879d8804bcde344eb4e1e38d3f952e9ac64b
daleyzou <daleyzou@163.com>
2020-11-24 15:16:00 +0800

    add GET Next

---
+++ src/java/com/daleyzou/CodeOffer/d57_GetNext.java
@@ -0,0 +1,30 @@
+package com.daleyzou.CodeOffer;
+
+/**
+ * d57_GetNext
+ * @description 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。
+ *              注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
+ * @author daleyzou
+ * @date 2020年11月24日 15:11
+ * @version 1.1.1
+ */
+public class d57_GetNext {
+    public class TreeLinkNode {
+        int val;
+
+        TreeLinkNode left = null;
+
+        TreeLinkNode right = null;
+
+        TreeLinkNode next = null;
+
+        TreeLinkNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+
+        return null;
+    }
+}
