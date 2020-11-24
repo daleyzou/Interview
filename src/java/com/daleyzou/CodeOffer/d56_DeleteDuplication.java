3010d7a2138876cc9c8450c13532037f31a065dd
daleyzou <daleyzou@163.com>
2020-11-24 15:11:13 +0800

    add deleteDuplication

--- src/java/com/daleyzou/CodeOffer/d56_DeleteDuplication.java
+++ src/java/com/daleyzou/CodeOffer/d56_DeleteDuplication.java
@@ -14,51 +14,50 @@ import java.util.Set;
 public class d56_DeleteDuplication {
     public class ListNode {
         int val;
+
         ListNode next = null;
 
         ListNode(int val) {
             this.val = val;
         }
     }
-    public ListNode deleteDuplication_1(ListNode pHead){
-        if (pHead == null || pHead.next == null){
+
+    public ListNode deleteDuplication_1(ListNode pHead) {
+        if (pHead == null || pHead.next == null) {
             return pHead;
         }
         ListNode pFirst = pHead;
         Set<Integer> set = new HashSet<>();
-        while (pFirst != null){
-           if (pFirst.next != null && pFirst.val == pFirst.next.val){
-               set.add(pFirst.val);
-           }
-           pFirst = pFirst.next;
+        while (pFirst != null) {
+            if (pFirst.next != null && pFirst.val == pFirst.next.val) {
+                set.add(pFirst.val);
+            }
+            pFirst = pFirst.next;
         }
         pFirst = pHead.next;
         ListNode pSecond = pHead;
-        while (pFirst != null){
-            if (set.contains(pFirst.val)){
+        while (pFirst != null) {
+            if (set.contains(pFirst.val)) {
                 pSecond.next = pFirst.next;
                 pFirst = pSecond.next;
-            }else {
+            } else {
                 pFirst = pFirst.next;
                 pSecond = pSecond.next;
             }
         }
-        if (pHead != null && set.contains(pHead.val)){
+        if (pHead != null && set.contains(pHead.val)) {
             pHead = pHead.next;
         }
 
         return pHead;
     }
 
-
-    public ListNode deleteDuplication_2(ListNode pHead)
-    {
+    public ListNode deleteDuplication_2(ListNode pHead) {
         // 用两个指针来做
-        if (pHead == null ||pHead.next == null){
+        if (pHead == null || pHead.next == null) {
             return pHead;
         }
         // 用三个指针来进行处理, 没写出来，失败
-
 
         return null;
     }
@@ -74,8 +73,61 @@ public class d56_DeleteDuplication {
             }
             return deleteDuplication(next);
         } else {
-            pHead.next = deleteDuplication(pHead.next);
+            pHead.next = deleteDuplication(next);
             return pHead;
         }
+    }
+
+    public ListNode deleteDuplication_3(ListNode pHead) {
+        if (pHead == null || pHead.next == null) {
+            return pHead;
+        }
+        ListNode head = new ListNode(-1);
+        ListNode trail = head;
+        while (pHead != null){
+            ListNode next = pHead.next;
+            boolean isSwap  = false;
+            while (next != null && pHead.val == next.val){
+                next = next.next;
+                isSwap = true;
+            }
+            if (!isSwap){
+                trail.next = pHead;
+                trail = trail.next;
+            }
+            pHead = next;
+        }
+        trail.next = null;
+        return head.next;
+    }
+
+//    public ListNode deleteDuplication(ListNode pHead) {
+//        if (pHead == null || pHead.next == null) {
+//            return pHead;
+//        }
+//
+//        ListNode head = new ListNode(-1); // 新建一个头结点，防止链表中头结点是重复节点被删除。
+//        ListNode trail = head;
+//
+//        while (pHead != null) {
+//            ListNode node = pHead.next;
+//            boolean flag = false;
+//            while (node != null && pHead.val == node.val) {
+//                node = node.next;
+//                flag = true;
+//            }
+//            if (!flag) {
+//                trail.next = pHead;
+//                trail = trail.next;
+//            }
+//            pHead = node;
+//        }
+//        trail.next = null; // 1->2->3->3->3
+//        return head.next;
+//    }
+
+    public static void main(String[] args) {
+        d56_DeleteDuplication object = new d56_DeleteDuplication();
+
     }
 }
