c9e181c218ddaa6e79db5440f416d978e34f8de0
daleyzou <daleyzou@163.com>
2020-11-25 23:02:50 +0800

    isSymmetrical

---
+++ src/java/com/daleyzou/CodeOffer/d58_IsSymmetrical.java
@@ -0,0 +1,46 @@
+package com.daleyzou.CodeOffer;
+
+/**
+ * d58_IsSymmetrical
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年11月25日 22:45
+ * @version 1.1.1
+ */
+public class d58_IsSymmetrical {
+
+    public class TreeNode {
+        int val = 0;
+
+        TreeNode left = null;
+
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+    boolean isSymmetrical(TreeNode pRoot) {
+        if (pRoot == null) {
+            return true;
+        }
+
+        return isSymmetrical(pRoot.left, pRoot.right);
+    }
+
+    private boolean isSymmetrical(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetrical(left.left, right.right) && isSymmetrical(left.right, right.left);
+    }
+}
