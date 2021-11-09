372a9054e0596d4112a333ee96e4c86273334f16
daleyzou <daleyzou@163.com>
2021-11-09 17:21:38 +0800

    add

--- src/java/leetcode/editor/cn/[76]最小覆盖子串.java
+++ src/java/leetcode/editor/cn/[76]最小覆盖子串.java
@@ -35,7 +35,54 @@ package leetcode.editor.cn;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution76 {
+
     public String minWindow(String s, String t) {
+        String result = "";
+        int[] need = new int[128];
+        int[] window = new int[128];
+        int needKind = 0;
+        int validate = 0;
+        int left = 0;
+        int right = 0;
+        for(int i = 0; i < t.length(); i++){
+            char needC = t.charAt(i);
+            if(need[needC] == 0){
+                needKind++;
+            }
+            need[needC]++;
+        }
+
+        while (right < s.length()){
+            char rightC = s.charAt(right);
+            if(need[rightC] > 0){
+                window[right]++;
+                if(need[rightC] == window[rightC]){
+                    validate++;
+                }
+            }
+
+            while(needKind == validate && left <= right){
+                if(needKind == validate){
+                    String temp = s.substring(left, right+1);
+                    result = result.length() < temp.length()?temp:result;
+                }
+
+                char leftC = s.charAt(left);
+                if(need[leftC] > 0){
+                    if(need[leftC] == window[leftC]){
+                        validate--;
+                    }
+                    window[leftC]--;
+                }
+                left++;
+            }
+            right++;
+        }
+
+        return result;
+    }
+
+    public String minWindow1(String s, String t) {
         if(t == null || t.length() < 1){
             return "";
         }
@@ -92,7 +139,7 @@ class Solution76 {
     public static void main(String[] args) {
         Solution76 solution76 = new Solution76();
 
-        String s = solution76.minWindow("ADOBECODEBANC",
+        String s = solution76.minWindow("BANC",
             "ABC");
         System.out.println("result: "+ s);
     }
