2a80c5ab3d9bdb3548ec2d59c1053ae077d3229a
daleyzou <daleyzou@163.com>
2022-01-17 00:47:30 +0800

    add

---
+++ src/java/leetcode/editor/cn/[15]三数之和.java
@@ -0,0 +1,105 @@
+package leetcode.editor.cn;//给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重
+//复的三元组。 
+//
+// 注意：答案中不可以包含重复的三元组。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [-1,0,1,2,-1,-4]
+//输出：[[-1,-1,2],[-1,0,1]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [0]
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= nums.length <= 3000 
+// -105 <= nums[i] <= 105 
+// 
+// Related Topics 数组 双指针 排序 
+// 👍 4203 👎 0
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        List<List<Integer>> data = new ArrayList<>();
+        int i = 0;
+        while(i < nums.length){
+            int cur = nums[i];
+            List<List<Integer>>  temp = twoSum(nums, i+1, 0 - cur);
+            for(List<Integer> it : temp){
+                it.add(nums[i]);
+                data.add(it);
+            }
+            while(i < nums.length && nums[i] == cur){
+                i++;
+            }
+        }
+        return data;
+    }
+
+    public List<List<Integer>> twoSum(int[] nums, int start, int target){
+        System.out.println("twoSum:" + target);
+        int begin = start;
+        int end = nums.length -1;
+        List<List<Integer>> result = new ArrayList<>();
+        while(begin < end){
+            int sum = nums[begin] + nums[end];
+            int left = nums[begin];
+            int right = nums[end];
+            if(sum < target){
+                while(begin < end && nums[begin] == left){
+                    begin++;
+                }
+            }else if(sum > target){
+                while(begin < end && nums[end] == right){
+                    end--;
+                }
+            }else {
+                List<Integer> list = new ArrayList<>();
+                list.add(nums[begin]);
+                list.add(nums[end]);
+                result.add(list);
+                while(begin < end && nums[begin] == left){
+                    begin++;
+                }
+                while(begin < end && nums[end] == right){
+                    end--;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<List<Integer>> lists = solution.threeSum(new int[]{1, -1, -1, 0});
+        System.out.println(lists);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
